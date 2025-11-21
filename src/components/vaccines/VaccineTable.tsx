1c089dfba622425dcd403b06d6f360ed25060d34
Ghitado <thiaguinho.020405@gmail.com>
2025-11-21 15:18:57 -0300

    feat(components/vaccines): add vaccine table component

---
+++ src/components/vaccines/VaccineTable.tsx
@@ -0,0 +1,56 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import type { VaccineResponse } from "../../api/vaccine";
+import ScrollableText from "../ScrollableText";
+
+export default function VaccineTable({
+  vaccines,
+}: {
+  vaccines: VaccineResponse[];
+}) {
+  return (
+    <TableContainer
+      component={Paper}
+      variant="outlined"
+      sx={{ maxHeight: 440 }}
+    >
+      <Table
+        stickyHeader
+        size="small"
+        sx={{ "& th, & td": { borderLeft: "1px solid #e0e0e0" } }}
+      >
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{ width: "100%" }}>Nome da Vacina</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {vaccines.length > 0 ? (
+            vaccines.map((v) => (
+              <TableRow key={v.id} hover>
+                <TableCell>
+                  <ScrollableText maxWidth="100%">
+                    <ScrollableText maxWidth={"50vh"}>{v.name}</ScrollableText>
+                  </ScrollableText>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell align="center" sx={{ py: 3, color: "text.secondary" }}>
+                Nenhuma vacina cadastrada.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
